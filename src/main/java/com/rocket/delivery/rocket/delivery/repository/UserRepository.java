fcc6b42d5342a3136ce500fd473a9956069a68af
dubraska primi <dubraskaprimi31@gmail.com>
2023-04-02 04:23:01 -0300

    coment

--- src/main/java/com/rocket/delivery/rocket/delivery/repository/UserRepository.java
+++ src/main/java/com/rocket/delivery/rocket/delivery/repository/UserRepository.java
@@ -3,7 +3,7 @@ package com.rocket.delivery.rocket.delivery.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rocket.delivery.rocket.delivery.entity.User;
-
+//conexiones que hacemos a la base de de datos 
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
